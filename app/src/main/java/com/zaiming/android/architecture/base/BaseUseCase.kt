873b50b977dd11e91a70f3235715af2d1cdcb72e
20156349 <qiuzaiming@mychery.com>
2024-02-27 09:56:43 +0800

    change BaseUseCase

--- app/src/main/java/com/zaiming/android/architecture/base/BaseUseCase.kt
+++ app/src/main/java/com/zaiming/android/architecture/base/BaseUseCase.kt
@@ -4,28 +4,43 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.coroutineScope
+import com.zaiming.android.architecture.base.interfaces.IDataCallback
+import com.zaiming.android.architecture.base.interfaces.IDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
-open class BaseUseCase : LifecycleOwner {
+abstract class BaseUseCase<ICB : IDataCallback> : LifecycleOwner, IDispatcher<ICB> {
 
   private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
   override val lifecycle: Lifecycle = lifecycleRegistry
+
+  /**
+   * Note: do not use in init{} scope because of dataCallback is not initialization.
+   */
+  protected var dataCallback: ICB? = null
+
+  final override fun setCallback(callback: ICB) {
+    dataCallback = callback
+  }
 
   init {
     lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
   }
 
-  open fun onReleaseAction() {
+  final override fun onReleaseAction() {
     lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    onReleaseInUseCase()
+    dataCallback = null
   }
+
+  open fun onReleaseInUseCase() {}
 
   fun launch(
     context: CoroutineContext = EmptyCoroutineContext,
-    operation: suspend CoroutineScope.() -> Unit
+    operation: suspend CoroutineScope.() -> Unit,
   ): Job {
     return lifecycle.coroutineScope.launch(context) {
       operation()
--- app/src/main/java/com/zaiming/android/architecture/business/usecases/actions/MainUseCase.kt
+++ app/src/main/java/com/zaiming/android/architecture/business/usecases/actions/MainUseCase.kt
@@ -4,17 +4,5 @@ import com.zaiming.android.architecture.base.BaseUseCase
 import com.zaiming.android.architecture.business.datacallback.MainCallback
 import com.zaiming.android.architecture.business.usecases.dispatchers.MainDispatcher
 
-class MainUseCase : BaseUseCase(), MainDispatcher {
-
-  private var callback: MainCallback? = null
-
-  override fun setCallback(callback: MainCallback) {
-    this.callback = callback
-  }
-
-  override fun onReleaseAction() {
-    super.onReleaseAction()
-    callback = null
-  }
-
+class MainUseCase : BaseUseCase<MainCallback>(), MainDispatcher {
 }
